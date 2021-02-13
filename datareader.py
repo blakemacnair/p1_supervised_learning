42b626927b7a821bef5b48ea41f0fd1f250fc2db
Blake Macnair <blakemacnair@gmail.com>
2021-02-13 17:11:52 -0500

    Renaming...

--- datareader.py
+++ datareader.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 
-def load_heart_data():
+def load_heart_data() -> pd.DataFrame:
     return pd.read_csv('data/heart_cleveland_upload.csv')
 
 
-def load_credit_card_data():
+def load_credit_card_data() -> pd.DataFrame:
     return pd.read_csv('data/creditcard.csv')
---
+++ decisiontree.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+
+if __name__ == "__main__":
+    load_heart_data()
+    pass
