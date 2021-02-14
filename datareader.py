e3d664732517a435696bdb645769b7276e1adfa8
Blake Macnair <blakemacnair@gmail.com>
2021-02-14 18:03:01 -0500

    Add optuna study optimization for full datasets.

--- datareader.py
+++ datareader.py
@@ -1,9 +1,45 @@
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def get_heart_train_test(train_size=0.6, random_state=None):
+    x, y = get_heart_xy()
+
+    return train_test_split(x,
+                            y,
+                            train_size=train_size,
+                            random_state=random_state,
+                            stratify=y)
+
+
+def get_credit_card_train_test(train_size=0.6, random_state=None):
+    x, y = get_credit_card_xy()
+
+    return train_test_split(x,
+                            y,
+                            train_size=train_size,
+                            random_state=random_state,
+                            stratify=y)
 
 
 def load_heart_data() -> pd.DataFrame:
     return pd.read_csv('data/heart_cleveland_upload.csv')
 
 
+def get_heart_xy() -> (np.ndarray, np.ndarray):
+    heart_data = load_heart_data()
+    x = heart_data.drop(columns=['condition'])
+    y = heart_data['condition']
+    return x, y
+
+
 def load_credit_card_data() -> pd.DataFrame:
     return pd.read_csv('data/creditcard.csv')
+
+
+def get_credit_card_xy() -> (np.ndarray, np.ndarray):
+    credit_card_data = load_credit_card_data()
+    x = credit_card_data.drop(columns=['Time', 'Class'])
+    y = credit_card_data['Class']
+    return x, y
--- decisiontree.py
+++ decisiontree.py
@@ -1,6 +1,102 @@
-import pandas as pd
-import numpy as np
+import pickle
+
+import optuna
+from sklearn.metrics import make_scorer, roc_auc_score
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+
+from datareader import get_credit_card_xy, get_heart_xy
+
+
+def objective_heart(trial):
+    x, y = get_heart_xy()
+    return objective(trial, x, y, make_scorer(roc_auc_score))
+
+
+def objective_credit_card(trial):
+    x, y = get_credit_card_xy()
+    return objective(trial, x, y, make_scorer(roc_auc_score))
+
+
+def objective(trial, x, y, scoring=None):
+    criterion = trial.suggest_categorical("criterion", ["gini", "entropy"])
+    max_depth = trial.suggest_int("max_depth", 6, 32)
+    min_samples_split = trial.suggest_float("min_samples_split", 0.01, 0.1)
+    min_samples_leaf = trial.suggest_float("min_samples_leaf", 0.01, 0.1)
+    min_impurity_decrease = trial.suggest_float("min_impurity_decrease", 0, 0.1)
+    ccp_alpha = trial.suggest_float("ccp_alpha", 0, 0.1)
+
+    dt = DecisionTreeClassifier(criterion=criterion,
+                                max_depth=max_depth,
+                                min_samples_split=min_samples_split,
+                                min_samples_leaf=min_samples_leaf,
+                                min_impurity_decrease=min_impurity_decrease,
+                                ccp_alpha=ccp_alpha,
+                                class_weight="balanced",
+                                random_state=42)
+
+    score = cross_val_score(dt, x, y, n_jobs=-1, cv=5, scoring=scoring)
+    return score.mean()
+
+
+def generate_dt_studies():
+    generate_dt_heart_study()
+    generate_dt_credit_card_study()
+
+
+def generate_dt_heart_study():
+    heart_study = optuna.create_study(direction="maximize")
+    heart_study.optimize(objective_heart, n_trials=100, show_progress_bar=True)
+
+    with open("models/dt/heart_study", "wb") as f:
+        pickle.dump(heart_study, f)
+
+
+def generate_dt_credit_card_study():
+    credit_card_study = optuna.create_study(direction="maximize")
+    credit_card_study.optimize(objective_credit_card, n_trials=100, show_progress_bar=True)
+
+    with open("models/dt/credit_card_study", "wb") as f:
+        pickle.dump(credit_card_study, f)
+
+
+def load_dt_models():
+    return load_dt_heart_model(), load_dt_credit_card_model()
+
+
+def load_dt_heart_model():
+    with open("models/dt/heart_study", "rb") as f:
+        study = pickle.load(f)
+
+    heart_best_params = study.best_params
+    return DecisionTreeClassifier(
+        criterion=heart_best_params["criterion"],
+        max_depth=heart_best_params["max_depth"],
+        min_samples_split=heart_best_params["min_samples_split"],
+        min_samples_leaf=heart_best_params["min_samples_leaf"],
+        min_impurity_decrease=heart_best_params["min_impurity_decrease"],
+        ccp_alpha=heart_best_params["ccp_alpha"],
+        class_weight="balanced",
+        random_state=42)
+
+
+def load_dt_credit_card_model():
+    with open("models/dt/credit_card_study", "rb") as f:
+        study = pickle.load(f)
+
+    heart_best_params = study.best_params
+    return DecisionTreeClassifier(
+        criterion=heart_best_params["criterion"],
+        max_depth=heart_best_params["max_depth"],
+        min_samples_split=heart_best_params["min_samples_split"],
+        min_samples_leaf=heart_best_params["min_samples_leaf"],
+        min_impurity_decrease=heart_best_params["min_impurity_decrease"],
+        ccp_alpha=heart_best_params["ccp_alpha"],
+        class_weight="balanced",
+        random_state=42)
+
 
 if __name__ == "__main__":
-    load_heart_data()
-    pass
+    # generate_dt_studies()
+    heart_dt, credit_card_dt = load_dt_models()
+
